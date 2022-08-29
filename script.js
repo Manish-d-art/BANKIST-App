964e0bdf2bc09b18646198105fb6786edcab1957
manish mandal <76776194mm@gmail.com>
2022-08-29 20:29:01 +0530

    update script.js

--- script.js
+++ script.js
@@ -63,10 +63,16 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 
 const displayMovements=function(movements){
-    movements.forEach(Function(mov,i){
-      const HTML=`
-      `
-
+  containerMovements.innerHTML='';
+    movements.forEach(function(mov,i){
+      const type=mov>0?'deposit':'withdrawal';
+      const html = `
+      <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+      `;
+      containerMovements.insertAdjacentHTML('afterbegin',html);
     });
 };
 
